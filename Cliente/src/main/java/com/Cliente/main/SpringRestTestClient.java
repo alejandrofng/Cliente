77874f2785cb8467aa3ffa9cd06d70b7f10ddec6
alejandrofng <alejandrofng@gmail.com>
2016-03-19 13:30:23 -0430

    >_> 404 Fixed

--- Cliente/src/main/java/com/Cliente/main/SpringRestTestClient.java
+++ Cliente/src/main/java/com/Cliente/main/SpringRestTestClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
  
 public class SpringRestTestClient {
  
-    public static final String REST_SERVICE_URI = "http://localhost:8090/IJMPiloto";
+    public static final String REST_SERVICE_URI = "http://localhost:8090/IJMpiloto";
      
     /* GET */
     @SuppressWarnings("unchecked")
